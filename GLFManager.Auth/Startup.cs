d09f3418d142ff2fed47862747b7568d154749e8
Shane McGuire <shanelgmcguire@gmail.com>
2021-05-12 22:29:13 -0600

    added (and debugging) docker compose file

--- GLFManager.Auth/Startup.cs
+++ GLFManager.Auth/Startup.cs
@@ -66,20 +66,7 @@ namespace GLFManager.Auth
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseIdentityServer();
         }
     }
 }
