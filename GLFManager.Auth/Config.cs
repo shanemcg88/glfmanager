5c05ad37fc5c196bd459964230221478a5905a63
Shane McGuire <shanelgmcguire@gmail.com>
2021-05-12 19:53:37 -0600

    added config and profileservices

--- GLFManager.Auth/Config.cs
+++ GLFManager.Auth/Config.cs
@@ -1,11 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 
 namespace GLFManager.Auth
 {
-    public class Config
+    public static class Config
     {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource("roles", new[] { JwtClaimTypes.Role })
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource
+                {
+                    Name = "glfapi",
+                    DisplayName = "GLF API",
+                    Scopes = { "glfapi.scope", JwtClaimTypes.Role }
+                }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("glfapi.scope", "GLF API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("UzKjRFnAHffxUFati8HMjSEzwMGgGHmN".Sha256())
+                    },
+                    AllowedScopes = { "glfapi.scope", "roles", IdentityServerConstants.StandardScopes.OpenId }
+                }
+            };
     }
 }
--- GLFManager.Auth/Services/ProfileService.cs
+++ GLFManager.Auth/Services/ProfileService.cs
@@ -1,11 +1,62 @@
-﻿using System;
+﻿using GLFManager.Models.Entities;
+using IdentityModel;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace GLFManager.Auth.Services
 {
-    public class ProfileService
+    public sealed class ProfileService : IProfileService
     {
+        private readonly IUserClaimsPrincipalFactory<User> _userClaimsPrincipalFactory;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public ProfileService (
+            UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IUserClaimsPrincipalFactory<User> userClaimsPrincipalFacotry)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _userClaimsPrincipalFactory = userClaimsPrincipalFacotry;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            // Get the user
+            string sub = context.Subject.GetSubjectId();
+            User user = await _userManager.FindByIdAsync(sub);
+
+            // Get the list of already assigned claims
+            ClaimsPrincipal userClaims = await _userClaimsPrincipalFactory.CreateAsync(user);
+            List<Claim> claims = userClaims.Claims.ToList();
+            claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
+
+            // Get the user's roles
+            IList<string> roles = await _userManager.GetRolesAsync(user);
+            foreach (var roleName in roles)
+            {
+                claims.Add(new Claim(JwtClaimTypes.Role, roleName)); // Add the role to the token claims
+                IdentityRole role = await _roleManager.FindByNameAsync(roleName);
+                if (role != null)
+                    claims.AddRange(await _roleManager.GetClaimsAsync(role)); // Roles can have a set of claims associated to them directly
+            }
+
+            context.IssuedClaims = claims;
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            string sub = context.Subject.GetSubjectId();
+            User user = await _userManager.FindByIdAsync(sub);
+            context.IsActive = user != null;
+        }
     }
 }
