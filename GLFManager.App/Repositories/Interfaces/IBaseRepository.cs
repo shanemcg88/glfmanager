4b44c625fa6405ad4a906fe781fbcb081a1d4cc6
shanemcg88 <shanelgmcguire@gmail.com>
2021-06-01 09:33:32 -0600

    added base repository for crud methods. still need to customize the delete function

---
+++ GLFManager.App/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLFManager.App.Repositories.Interfaces
+{
+    public interface IBaseRepository<TEntity, TPrimaryKey>
+    {
+        Task<TEntity> Create(TEntity src);
+        Task<TEntity> Get(TPrimaryKey id);
+        Task<List<TEntity>> GetAll();
+        Task Delete(Guid id);
+    }
+}
--- GLFManager.Models/ViewModels/Companies/CompanyViewModel.cs
+++ GLFManager.Models/ViewModels/Companies/CompanyViewModel.cs
@@ -6,6 +6,16 @@ namespace GLFManager.Models.ViewModels.Companies
 {
     public class CompanyViewModel
     {
-
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string OfficePhone { get; set; }
+        public string OfficeEmail { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public string Contact { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactEmail { get; set; }
     }
 }
