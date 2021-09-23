3717e3071ec8dda781c21ea2b30984fd7106dd98
smc-dotexe <shanelgmcguire@gmail.com>
2021-09-22 23:26:15 -0600

    learning angular forms for login

--- ClientApp/projects/glfclient/src/app/login/login.component.ts
+++ ClientApp/projects/glfclient/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
   }
 
 }
