3e41d2ec4dc4f7af59d7121640d85ff1483d0958
shanemcguire <shane.mcguire@vogappdevelopers.com>
2022-03-01 23:48:43 -0700

    added pagination function

--- svelte_client/client/src/components/tables/ContentTable.svelte
+++ svelte_client/client/src/components/tables/ContentTable.svelte
@@ -1,15 +1,16 @@
 <script>
-    import { onMount } from "svelte";
+import { onMount } from "svelte";
 
     export let tableSettings;
     export let tableContent;
 
-    let numberOfDataDisplayed = 1;
-    let dataDisplayed = $tableContent;
-
+    let numberOfDataDisplayed = 3;
+    $: dataDisplayed = [];
+    let data = $tableContent;
     // Pagination variables. Fix showData to dataDisplayed?
     let pageCounter = 0;
     var showData = [];
+    let pages = 0;
 
     function setDisplayNumber(displayNum) {
 
@@ -29,7 +30,7 @@
     function sortColumn(index, column, isSorted) {
 
         let columnData = [];
-        dataDisplayed.forEach((x) =>{
+        data.forEach((x) =>{
             columnData.push(x[column]);
         })
 
@@ -45,68 +46,49 @@
         let newSortedData = [];
 
         columnData.forEach(x => {
-            var match = dataDisplayed.find(data => data[column] === x);
+            var match = data.find(data => data[column] === x);
             newSortedData.push(match);
         });
 
-        dataDisplayed = dataDisplayed = newSortedData;
-
+        data = data = newSortedData;
+        pagination(1);
     }
 
-    function pagination(num) {
-        console.log('pagination ran');
+    function pagination(pageSelected) {
 
-        let innerIter=0
-        let iterateNum = 0;
-        for(let i = 0; i < $tableContent.length; i++) {
-            console.log('ITERATE NUM START', iterateNum);
-            /*
-                we want 3 items per page 
-                numberdatadisplayed * 1 = 3 -> 0, 1, 2
-                    var iter = numberdatadisplay * i - 1
-                        loop through tablecontent[0][1][2]
-                        for(let x=0; x < iter; x++) {
-                            tablecontent[x]
-                        }
+        let end = pageSelected * numberOfDataDisplayed - 1;
+        let start = end - numberOfDataDisplayed + 1
 
-                numbedatadisplayed * 2 = 6 -> 3, 4, 5
-                    var iter = numberdatadisplay * i - 1;
-                    loop through tablecontent[3][4][5]
-                    for(let x=iter-numberdatadisplayed; x < iter; x++)
-                numberdatadisplayed * 3 = 9 -> 6, 7, 8
-            */
-            // if (numberOfDataDisplayed === 1) {
-            //     iterateNum = 1;
-            // }
-            if ((numberOfDataDisplayed * i - 1) <= 0) {
-                console.log('this if ran?');
-                iterateNum = numberOfDataDisplayed;
-            }
-            else {
-                let calc = numberOfDataDisplayed * i - 1;
-                console.log(`iterateNum ${iterateNum} += ${calc} `)
-                iterateNum += calc;
-            }
+        let currentDisplay=[];
 
-            console.log('iterateNum BEFORE', iterateNum);
-            for(let x = iterateNum - numberOfDataDisplayed; x <= iterateNum-1; x++) {
-                if (iterateNum === 1 && i > 0) {
-                    x = 1;
-                }
-                console.log('X=', x);
-                console.log("CONTENT", $tableContent[x]);
-            }
-            console.log('ITERATENUM AFTER', iterateNum);
-            
+        for (let x = start; x <= end; x++) {
+
+            if (data[x])
+                currentDisplay.push(data[x]);
+            else
+                break;
+
         }
 
+        dataDisplayed = currentDisplay;
+    }
 
-        console.log('pageCounter=', pageCounter);
+    function pageCalculator() {
+
+        let totalData = $tableContent.length;
+        let calc = Math.floor(totalData / numberOfDataDisplayed);
+
+        if (totalData % numberOfDataDisplayed > 0) {
+            calc = calc + 1;
+        }
+
+        return pages = calc;
     }
 
     onMount(() => {
-        setDisplayNumber(numberOfDataDisplayed)
-        pagination();
+        setDisplayNumber(numberOfDataDisplayed);
+        pageCalculator();
+        pagination(1);
     })
 
 </script>
@@ -145,6 +127,6 @@
     <option value = 3> 3 </option>
 </select>
 
-{#each showData as data}
-
+{#each Array(pages) as _, i}
+    <li on:click={()=>pagination(i+1)}>{i+1}</li>
 {/each}
